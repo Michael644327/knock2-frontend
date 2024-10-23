ef53199d01ad888f7ac910ac192944e023e40dd0
Michael644327 <keinsacer@gmail.com>
2024-10-23 17:52:13 +0800

    init 3

--- configs/index.js
+++ configs/index.js
@@ -2,8 +2,11 @@ export const PORT = 6005
 export const DEV = true
 
 // express 的位置
-export const apiBaseUrl = 'http://localhost:3005/api'
-export const avatarBaseUrl = 'http://localhost:3005/avatar'
+export const apiBaseUrl = 'https://knock2-backend.vercel.app/api'
+export const avatarBaseUrl = 'https://knock2-backend.vercel.app/avatar'
+
+// export const apiBaseUrl = 'http://localhost:3005/api'
+// export const avatarBaseUrl = 'http://localhost:3005/avatar'
 
 // export const API_SERVER = 'http://localhost:3001'
 // export const THEME_LIST = `${API_SERVER}/themes/api`
